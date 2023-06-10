63ca18b597d1f7f79d76c2e0ad3f9bf8813efb6a
andallves <andrealves10a@gmail.com>
2023-06-10 20:21:44 -0300

    create a FirstStep component

---
+++ src/pages/FirstStep/index.tsx
@@ -0,0 +1,51 @@
+import { CardForm } from '../../layout/CardForm';
+import { Button } from '../../Components/Button';
+import { ButtonContainer } from '../../Components/ButtonContainer';
+import { DivSteps } from '../../Components/DivSteps';
+import { Divider } from '../../Components/Divider';
+import { InputContainer } from '../../Components/InputContainer';
+import { InputField } from '../../Components/InputField';
+
+import * as Styled from './styles';
+
+export const FirstStep = () => {
+  return (
+    <Styled.Container>
+      <CardForm>
+        <DivSteps />
+        <Divider />
+        <InputContainer>
+          <InputField
+            text={'Nome'}
+            name={'nome'}
+            type={'text'}
+            placeholder={'Como prefere ser chamado'}
+          />
+
+          <InputField
+            text={'Telefone'}
+            name={'telefone'}
+            type={'number'}
+            placeholder={'Digite seu número de WhatsApp '}
+          />
+
+          <InputField
+            text={'Email'}
+            name={'email'}
+            type={'email'}
+            placeholder={'Digite seu e-mail'}
+          />
+        </InputContainer>
+
+        <ButtonContainer>
+          <Button
+            btnText="Continuar"
+            isDark={false}
+            isUppercase={true}
+            isBold={true}
+          />
+        </ButtonContainer>
+      </CardForm>
+    </Styled.Container>
+  );
+};
