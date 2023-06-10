52a4790868ab6ddfe87b81f4c170250734c80153
andallves <andrealves10a@gmail.com>
2023-06-10 13:52:08 -0300

    update a CardForm component including a new field

--- src/layout/CardForm/index.tsx
+++ src/layout/CardForm/index.tsx
@@ -1,7 +1,9 @@
 import { Button } from '../../Components/Button';
+import { ButtonContainer } from '../../Components/ButtonContainer';
 import { DivSteps } from '../../Components/DivSteps';
 import { Divider } from '../../Components/Divider';
 import { InputContainer } from '../../Components/InputContainer';
+import { InputField } from '../../Components/InputField';
 import * as Styled from './styles';
 
 export const CardForm = () => {
@@ -10,8 +12,36 @@ export const CardForm = () => {
       <DivSteps />
       <Divider />
       <InputContainer>
+        <InputField
+          text={'Nome'}
+          name={'nome'}
+          type={'text'}
+          placeholder={'Como prefere ser chamado'}
+        />
+
+        <InputField
+          text={'Telefone'}
+          name={'telefone'}
+          type={'number'}
+          placeholder={'Digite seu número de WhatsApp '}
+        />
+        
+        <InputField
+          text={'Email'}
+          name={'email'}
+          type={'email'}
+          placeholder={'Digite seu e-mail'}
+        />
       </InputContainer>
-      <Button btnText='Continuar' isDark={false} isUppercase={true} isBold={true} />
+
+      <ButtonContainer>
+        <Button
+          btnText="Continuar"
+          isDark={false}
+          isUppercase={true}
+          isBold={true}
+        />
+      </ButtonContainer>
     </Styled.Container>
-  )
-}
+  );
+};
