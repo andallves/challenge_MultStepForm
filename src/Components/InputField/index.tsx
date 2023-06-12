487fe75bf4ed0812a8fcef9af04241c7579eb8a5
andallves <andrealves10a@gmail.com>
2023-06-12 13:17:07 -0300

    editing Select component

--- src/Components/InputField/index.tsx
+++ src/Components/InputField/index.tsx
@@ -1,5 +1,4 @@
-import { Input } from '../Input';
-import { Text } from '../Text';
+import { useStepContext } from '../../contexts/StepContext';
 import * as Styled from './styles';
 
 interface InputField {
@@ -7,13 +6,21 @@ interface InputField {
   name: string;
   type: string;
   placeholder: string;
+  updateFieldHandler: (name: string, target: string) => {};
 }
 
-export const InputField = ({ text, name, type, placeholder }: InputField) => {
+export const InputField = ({ text, name, type, placeholder, updateFieldHandler}: InputField) => {
+  const [dispatch] = useStepContext()
   return (
     <Styled.Container>
-      <Text text={text} isDark={true} isUppercase={false} isBold={true} />
-      <Input name={name} type={type} placeholder={placeholder} />
+      <label htmlFor={name}>{text}</label>
+      <input
+        type={type}
+        name={name}
+        id={name}
+        placeholder={placeholder}
+        onChange={updateFieldHandler)}
+      />
     </Styled.Container>
   );
 };
--- src/Components/InputField/styles.ts
+++ src/Components/InputField/styles.ts
@@ -6,7 +6,7 @@ export const Container = styled.div`
     flex-direction: column;
     align-items: flex-start;
     padding: 0;
-    gap: .4rem;
+    gap: 0.4rem;
 
     width: 52.8rem;
     height: 8.4rem;
@@ -15,5 +15,65 @@ export const Container = styled.div`
     order: 0;
     align-self: stretch;
     flex-grow: 0;
+
+    > label {
+      ${({ theme }) => css`
+        width: 15.2rem;
+        height: 2.4rem;
+
+        font-family: #{theme.fontFamily.default};
+        font-style: normal;
+        font-weight: ${theme.fontTypes.bold};
+        font-size: 1.6rem;
+        line-height: 150%;
+
+        color: ${theme.colors.gray40};
+
+        flex: none;
+        order: 0;
+        flex-grow: 0;
+      `}
+    }
+    > input {
+      ${({ theme }) => css`
+        box-sizing: border-box;
+
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        padding: 0.8rem 1.6rem;
+        gap: 1rem;
+
+        width: 52.8rem;
+        height: 5.6rem;
+
+        background: ${theme.colors.white};
+        border: 0.1rem solid ${theme.colors.gray10};
+        border-radius: 0.2rem;
+
+        flex: none;
+        order: 1;
+        align-self: stretch;
+        flex-grow: 0;
+
+        &::placeholder {
+          ${({ theme }) => css`
+            height: 2.4rem;
+
+            font-family: ${theme.fontFamily.default};
+            font-style: normal;
+            font-weight: ${theme.fontTypes.regular};
+            font-size: 1.6rem;
+            line-height: 150%;
+
+            color: ${theme.colors.gray20};
+
+            flex: none;
+            order: 0;
+            flex-grow: 0;
+          `};
+        }
+      `}
+    }
   `}
 `;
