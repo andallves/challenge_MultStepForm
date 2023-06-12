43ce031e38d5e6863d2cbc3756cdc00bfc050ff0
andallves <andrealves10a@gmail.com>
2023-06-12 20:04:35 -0300

    fix some bugs in onButtonClicked

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,60 +1,59 @@
 import { Button } from '../Components/Button';
-import { ButtonContainer } from '../Components/ButtonContainer';
 import { DivSteps } from '../Components/DivSteps';
 import { Divider } from '../Components/Divider';
-import { useStepContext } from '../contexts/StepContext';
-import { userForm } from '../hooks/userForm';
+import { useAppContext } from '../contexts/AppContext';
 import { CardForm } from '../layout/CardForm';
 import { FirstStep } from '../pages/FirstStep';
 
-
 function App() {
-  const [state, actions] = useStepContext();
-  
+  const { state, nextStep } = useAppContext();
+
+  const isFirstStep = true;
+  const isLastStep = false;
   return (
     <main>
       <CardForm>
-        <DivSteps currentStep={state.stepId}/>
+        <DivSteps currentStep={state.stepId} />
         <Divider />
 
-        <FirstStep updateFieldHandler={actions.update}/>
+        <FirstStep />
 
+        {isFirstStep && (
+          <Button
+            nameofClass={'next'}
+            type={'button'}
+            btnText="Continuar"
+            isDark={false}
+            isUppercase={true}
+            isBold={true}
+            disabled={false}
+            onButtonClicked={() => nextStep}
+          />
+        )}
 
-          {!isFirstStep && (
-            <Button
-              nameofClass={'continuar'}
-              type={'button'}
-              btnText="Continuar"
-              isDark={false}
-              isUppercase={true}
-              isBold={true}
-              disabled={false}
-              onButtonClicked={actions.next}
-            />
-          )}
-
-          {!isLastStep ? (
-            <Button
-              nameofClass={'continuar'}
-              type={'button'}
-              btnText="Continuar"
-              isDark={false}
-              isUppercase={true}
-              isBold={true}
-              disabled={false}
-              onButtonClicked={actions.previous}
-            />
-          ) : (
-            <Button
-              type={'submit'}
-              btnText="Enviar Proposta"
-              isDark={false}
-              isUppercase={true}
-              isBold={true}
-              disabled={false}
-              onButtonClicked={actions.previous}
-            />
-          )}
+        {!isLastStep ? (
+          <Button
+            nameofClass={'continuar'}
+            type={'button'}
+            btnText="Continuar"
+            isDark={false}
+            isUppercase={true}
+            isBold={true}
+            disabled={false}
+            onButtonClicked={() => nextStep}
+          />
+        ) : (
+          <Button
+            nameofClass={'continuar'}
+            type={'submit'}
+            btnText="Enviar Proposta"
+            isDark={false}
+            isUppercase={true}
+            isBold={true}
+            disabled={false}
+            onButtonClicked={() => nextStep}
+          />
+        )}
       </CardForm>
     </main>
   );
