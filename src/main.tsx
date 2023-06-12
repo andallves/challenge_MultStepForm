536ca388382b0f807049a4cac50b9464ada9fc85
andallves <andrealves10a@gmail.com>
2023-06-12 19:59:27 -0300

    editing function name to AppContexProvider

--- src/main.tsx
+++ src/main.tsx
@@ -4,16 +4,16 @@ import { ThemeProvider } from 'styled-components';
 
 import { GlobalStyles } from './styles/global-styles';
 import { theme } from './styles/theme';
-import { StepContextProvider } from './contexts/StepContext/index.tsx';
+import { AppContextProvider } from './contexts/AppContext/index.tsx';
 import { AppRoutes } from './routes/index.tsx';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <StepContextProvider>
+    <AppContextProvider>
       <ThemeProvider theme={theme}>
         <AppRoutes />
         <GlobalStyles />
       </ThemeProvider>
-    </StepContextProvider>
+    </AppContextProvider>
   </React.StrictMode>,
 );
