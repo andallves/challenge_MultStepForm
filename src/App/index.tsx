e53dfcee37df7fd88cff8e135b1763a37d5696ee
andallves <andrealves10a@gmail.com>
2023-06-12 13:16:18 -0300

    editing App component

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,10 +1,69 @@
+import { Button } from '../Components/Button';
+import { ButtonContainer } from '../Components/ButtonContainer';
+import { DivSteps } from '../Components/DivSteps';
+import { Divider } from '../Components/Divider';
+import { useStepContext } from '../contexts/StepContext';
+import { userForm } from '../hooks/userForm';
+import { CardForm } from '../layout/CardForm';
 import { FirstStep } from '../pages/FirstStep';
 
+
 function App() {
+  const [state, actions] = useStepContext();
+
+  const updateFieldHandler = (key: string, value: string) => {
+    actions.update(key: value)
+  }
+
+  const { currentStep, currentComponent, changeStep, isLastStep, isFirstStep } =
+    userForm(formComponents);
+
   return (
-    <>
-      <FirstStep />
-    </>
+    <main>
+      <CardForm>
+        <DivSteps currentStep={state}/>
+        <Divider />
+
+        <FirstStep updateFieldHandler={actions.update}/>
+
+
+          {!isFirstStep && (
+            <Button
+              nameofClass={'continuar'}
+              type={'button'}
+              btnText="Continuar"
+              isDark={false}
+              isUppercase={true}
+              isBold={true}
+              disabled={false}
+              onButtonClicked={actions.next}
+            />
+          )}
+
+          {!isLastStep ? (
+            <Button
+              nameofClass={'continuar'}
+              type={'button'}
+              btnText="Continuar"
+              isDark={false}
+              isUppercase={true}
+              isBold={true}
+              disabled={false}
+              onButtonClicked={actions.previous}
+            />
+          ) : (
+            <Button
+              type={'submit'}
+              btnText="Enviar Proposta"
+              isDark={false}
+              isUppercase={true}
+              isBold={true}
+              disabled={false}
+              onButtonClicked={actions.previous}
+            />
+          )}
+      </CardForm>
+    </main>
   );
 }
 
