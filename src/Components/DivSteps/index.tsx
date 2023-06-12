2f9d5a1a6f781a744e45f2806f4e087b9f1aa69d
andallves <andrealves10a@gmail.com>
2023-06-12 20:06:54 -0300

    fix some bugs

--- src/Components/DivSteps/index.tsx
+++ src/Components/DivSteps/index.tsx
@@ -2,32 +2,38 @@ import * as Styled from './styles';
 import { IoIosArrowForward } from 'react-icons/io';
 import { Steps } from '../Steps';
 import { Select } from '../Select';
-import { useStepContext } from '../../contexts/StepContext';
-import { ReducerProps } from '../../contexts/StepContext/reducer';
+import { useAppContext } from '../../contexts/AppContext';
 
-export const DivSteps = ({currentStep}) => {
-  const [state, action] = useStepContext<ReducerProps[]>();
+export const DivSteps = ({ currentStep }) => {
+  const { state } = useAppContext();
+
+
   return (
     <Styled.Container>
-        <Steps>
-          <Select status={'active'} stepText={'1'} />
-          <p className="text">Contato</p>
-        </Steps>
+      <Steps>
+        <Select status={'active'} stepText={`${state.stepId}`} />
+        <p className="text">Contato</p>
+      </Steps>
 
       <IoIosArrowForward />
 
       <Steps>
-        <Select status={currentStep >= 1 ? 'active' : 'disable'} stepText={'2'} />
+        <Select
+          status={currentStep >= 1 ? 'active' : 'disable'}
+          stepText={'2'}
+        />
         <p className="text">Empresa</p>
       </Steps>
 
       <IoIosArrowForward />
 
       <Steps>
-        <Select status={currentStep >= 2 ? 'active' : 'disable'} stepText={'3'} />
+        <Select
+          status={currentStep >= 2 ? 'active' : 'disable'}
+          stepText={'3'}
+        />
         <p className="text">Projeto</p>
       </Steps>
     </Styled.Container>
-
   );
 };
