1e5fc6d9cf9e7c7c5970f106c822e4320826fa9c
andallves <andrealves10a@gmail.com>
2023-06-12 20:01:02 -0300

    removing the props and adding a class to button

--- src/Components/Button/styles.ts
+++ src/Components/Button/styles.ts
@@ -8,7 +8,7 @@ interface Container {
   isDark: boolean;
 }
 export const Container = styled.div`
-  ${({theme, nameofClass}) => css`
+  ${({ theme }) => css`
     display: flex;
     flex-direction: row;
     justify-content: flex-end;
@@ -24,43 +24,45 @@ export const Container = styled.div`
     align-self: stretch;
     flex-grow: 0;
 
-  > button {
-      display: flex;
-      flex-direction: row;
-      justify-content: center;
-      align-items: center;
-      padding: 1.6rem 3.2rem;
-      gap: 0.8rem;
 
-      width: 15.8rem;
-      height: 5.6rem;
+      > button .next {
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        align-items: center;
+        padding: 1.6rem 3.2rem;
+        gap: 0.8rem;
 
-      /* Product/purple-light */
-      background: ${theme.colors.purpleLight};
-      box-shadow: 0 0.4rem 0.4rem rgba(0, 0, 0, 0.25);
-      border-radius: 0.6rem;
-      border: none;
+        width: 15.8rem;
+        height: 5.6rem;
 
-      /* Inside auto layout */
-      flex: none;
-      order: 0;
-      flex-grow: 0;
+        /* Product/purple-light */
+        background: ${theme.colors.purpleLight};
+        box-shadow: 0 0.4rem 0.4rem rgba(0, 0, 0, 0.25);
+        border-radius: 0.6rem;
+        border: none;
 
-      > span {
-        width: 9.4rem;
-        height: 2.4rem;
-
-        font-style: normal;
-        font-weight: ${theme.fontTypes.bold};
-        font-size: 1.6rem;
-        line-height: 150%;
-        text-transform: uppercase;
-        color: ${theme.colors.gray40};
-
+        /* Inside auto layout */
         flex: none;
-        order: 1;
+        order: 0;
         flex-grow: 0;
+
+        > span {
+          width: 9.4rem;
+          height: 2.4rem;
+
+          font-style: normal;
+          font-weight: ${theme.fontTypes.bold};
+          font-size: 1.6rem;
+          line-height: 150%;
+          text-transform: uppercase;
+          color: ${theme.colors.gray40};
+
+          flex: none;
+          order: 1;
+          flex-grow: 0;
+        }
       }
-    }
+
   `}
 `;
