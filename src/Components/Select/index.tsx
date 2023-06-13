b73496bea6870fcdf86ec74775b7dbe09be7de84
andallves <andrealves10a@gmail.com>
2023-06-13 12:31:45 -0300

    editing component to fix bug of the layout

--- src/Components/Select/index.tsx
+++ src/Components/Select/index.tsx
@@ -1,13 +1,15 @@
 import * as Styled from './styles';
 
 interface Select {
-  status: string;
+  status: () => string | undefined;
   stepText: string;
 }
 export const Select = ({ status, stepText }: Select) => {
   return (
-    <Styled.Container className={status} >
-      <p>{stepText}</p>
+    <Styled.Container>
+      <div className={status()}>
+        <p>{stepText}</p>
+      </div>
     </Styled.Container>
   );
 };
