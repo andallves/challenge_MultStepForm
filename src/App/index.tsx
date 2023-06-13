20b1e8bea22f47800db6301681126db4cb62357c
andallves <andrealves10a@gmail.com>
2023-06-12 23:50:03 -0300

    includes new functionality and correct some bugs

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,60 +1,79 @@
 import { Button } from '../Components/Button';
+import { ButtonContainer } from '../Components/ButtonContainer';
 import { DivSteps } from '../Components/DivSteps';
 import { Divider } from '../Components/Divider';
-import { useAppContext } from '../contexts/AppContext';
+import { userForm } from '../hooks/userForm';
 import { CardForm } from '../layout/CardForm';
 import { FirstStep } from '../pages/FirstStep';
+import { SecondStep } from '../pages/SecondStep';
+import { ThirdStep } from '../pages/ThirdStep';
+
+import * as Styled from './styles';
 
 function App() {
-  const { state, nextStep } = useAppContext();
+  const steps: React.ReactNode[] = [
+    <FirstStep />,
+    <SecondStep />,
+    <ThirdStep />,
+  ];
 
-  const isFirstStep = true;
-  const isLastStep = false;
+  const { currentStep, currentComponent, changeStep, isLastStep, isFirstStep } =
+    userForm(steps);
+
   return (
     <main>
-      <CardForm>
-        <DivSteps currentStep={state.stepId} />
-        <Divider />
+      <Styled.Container>
+        <CardForm>
+          <DivSteps currentStep={currentStep} />
+          <Divider />
+          {
+            <form
+              onSubmit={(event: React.FormEvent<HTMLFormElement>): void =>
+                changeStep(currentStep + 1, event)
+              }
+            >
+              {currentComponent}
 
-        <FirstStep />
+              <ButtonContainer>
+                {!isFirstStep && (
+                  <Button
+                    nameofClass={'prev'}
+                    type={'button'}
+                    btnText="Voltar"
+                    isDark={false}
+                    isUppercase={true}
+                    isBold={true}
+                    disabled={false}
+                    onButtonClicked={() => changeStep(currentStep - 1)}
+                  />
+                )}
 
-        {isFirstStep && (
-          <Button
-            nameofClass={'next'}
-            type={'button'}
-            btnText="Continuar"
-            isDark={false}
-            isUppercase={true}
-            isBold={true}
-            disabled={false}
-            onButtonClicked={() => nextStep}
-          />
-        )}
-
-        {!isLastStep ? (
-          <Button
-            nameofClass={'continuar'}
-            type={'button'}
-            btnText="Continuar"
-            isDark={false}
-            isUppercase={true}
-            isBold={true}
-            disabled={false}
-            onButtonClicked={() => nextStep}
-          />
-        ) : (
-          <Button
-            nameofClass={'continuar'}
-            type={'submit'}
-            btnText="Enviar Proposta"
-            isDark={false}
-            isUppercase={true}
-            isBold={true}
-            disabled={false}
-            onButtonClicked={() => nextStep}
-          />
-        )}
-      </CardForm>
+                {!isLastStep ? (
+                  <Button
+                    nameofClass={'next'}
+                    type={'submit'}
+                    btnText="Continuar"
+                    isDark={false}
+                    isUppercase={true}
+                    isBold={true}
+                    disabled={false}
+                  />
+                ) : (
+                  <Button
+                    nameofClass={'next'}
+                    type={'button'}
+                    btnText="Enviar"
+                    isDark={false}
+                    isUppercase={true}
+                    isBold={true}
+                    disabled={false}
+                  />
+                )}
+              </ButtonContainer>
+            </form>
+          }
+        </CardForm>
+      </Styled.Container>
     </main>
   );
 }
