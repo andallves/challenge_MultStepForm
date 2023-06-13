1303b057d3d4ad0155b736259689da8339400776
andallves <andrealves10a@gmail.com>
2023-06-13 12:32:22 -0300

    correcting layout

--- src/layout/CardForm/styles.ts
+++ src/layout/CardForm/styles.ts
@@ -3,7 +3,7 @@ import styled, { css } from 'styled-components';
 export const Container = styled.div`
   ${({ theme }) => css`
     box-sizing: border-box;
-    
+
     display: flex;
     flex-direction: column;
     align-items: flex-start;
@@ -12,9 +12,9 @@ export const Container = styled.div`
 
     position: absolute;
     width: 59.2rem;
-    height: 54.4rem;
+    height: 58.8rem;
     left: calc(50% - 59.2rem/2);
-    top: calc(50% - 54.4rem/2);
+    top: calc(50% - 58.8rem/2);
 
     background: ${theme.colors.white};
     border: .1rem solid ${theme.colors.gray10};
