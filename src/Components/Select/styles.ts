f753a662e8e0bc00e46d78a72f6c19a9fb399c6a
andallves <andrealves10a@gmail.com>
2023-06-13 12:31:22 -0300

    fix layout bug

--- src/Components/Select/styles.ts
+++ src/Components/Select/styles.ts
@@ -1,8 +1,46 @@
 import styled, { css } from 'styled-components';
 
 export const Container = styled.div`
-  > div.select {
-    ${({ theme }) => css`
+  ${({ theme }) => css`
+    div.disabled  {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      padding: 0.8rem 1rem;
+      gap: 1rem;
+
+      width: 2.8rem;
+      height: 2.8rem;
+
+      background: ${theme.colors.gray10};
+      border-radius: 50%;
+
+      flex: none;
+      order: 0;
+      flex-grow: 0;
+
+      /* font styles */
+      > p {
+        font-family: 'Titillium Web';
+        font-style: normal;
+        font-weight: ${theme.fontTypes.bold};
+        font-size: 1.4rem;
+        line-height: 150%;
+
+        leading-trim: both;
+        text-edge: cap;
+        text-align: center;
+
+        color: ${theme.colors.gray30};
+
+        flex: none;
+        order: 0;
+        flex-grow: 0;
+      }
+    }
+
+    > div.select {
       display: flex;
       flex-direction: column;
       justify-content: center;
@@ -38,11 +76,9 @@ export const Container = styled.div`
         order: 0;
         flex-grow: 0;
       }
-    `}
-  }
+    }
 
-  div.active {
-    ${({ theme }) => css`
+    > div.active {
       display: flex;
       flex-direction: column;
       justify-content: center;
@@ -53,7 +89,7 @@ export const Container = styled.div`
       width: 2.8rem;
       height: 2.8rem;
 
-      background: ${theme.colors.sucess};
+      background: ${theme.colors.sucessColor};
       border-radius: 50%;
 
       flex: none;
@@ -78,46 +114,6 @@ export const Container = styled.div`
         order: 0;
         flex-grow: 0;
       }
-    `};
-
-    > div.disabled {
-      ${({ theme }) => css`
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        padding: 0.8rem 1rem;
-        gap: 1rem;
-
-        width: 2.8rem;
-        height: 2.8rem;
-
-        background: ${theme.colors.gray20};
-        border-radius: 50%;
-
-        flex: none;
-        order: 0;
-        flex-grow: 0;
-
-        /* font styles */
-        > p {
-          font-family: 'Titillium Web';
-          font-style: normal;
-          font-weight: ${theme.fontTypes.bold};
-          font-size: 1.4rem;
-          line-height: 150%;
-
-          leading-trim: both;
-          text-edge: cap;
-          text-align: center;
-
-          color: ${theme.colors.gray40};
-
-          flex: none;
-          order: 0;
-          flex-grow: 0;
-        }
-      `};
     }
-  }
+  `}
 `;
