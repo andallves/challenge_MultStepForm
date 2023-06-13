7c8f5f5b1422ea8368dff0fcb83509d3bb90ad61
andallves <andrealves10a@gmail.com>
2023-06-13 13:46:27 -0300

    add all the functionality in the reducer

--- src/contexts/AppContext/reducer.ts
+++ src/contexts/AppContext/reducer.ts
@@ -7,15 +7,34 @@ export const reducer = (
   action: IDispatchAction,
 ): InitialStateType => {
   switch (action.type) {
-    case AppContextActions.NextStep: {
-      return { ...state, stepId: state.stepId + 1 };
-    }
-    case AppContextActions.PrevStep: {
-      return { ...state, stepId: state.stepId - 1 };
-    }
-    case AppContextActions.UpdateData: {
+
+    case AppContextActions.UpdateName: {
       if ( typeof action.payload === 'undefined') return { ...state }
       return { ...state, name: action.payload };
+    }
+    case AppContextActions.UpdateTel: {
+      if ( typeof action.payload === 'undefined') return { ...state }
+      return { ...state, tel: action.payload };
+    }
+    case AppContextActions.UpdateEmail: {
+      if ( typeof action.payload === 'undefined') return { ...state }
+      return { ...state, email: action.payload };
+    }
+    case AppContextActions.UpdateNameBusiness: {
+      if ( typeof action.payload === 'undefined') return { ...state }
+      return { ...state, nameBusiness: action.payload };
+    }
+    case AppContextActions.UpdateQtdFuncionarios: {
+      if ( typeof action.payload === 'undefined') return { ...state }
+      return { ...state, qtdFuncionarios: action.payload };
+    }
+    case AppContextActions.UpdateAboutBusiness: {
+      if ( typeof action.payload === 'undefined') return { ...state }
+      return { ...state, aboutBusiness: action.payload };
+    }
+    case AppContextActions.UpdateAboutProject: {
+      if ( typeof action.payload === 'undefined') return { ...state }
+      return { ...state, aboutProject: action.payload };
     }
     default:
       throw new Error("Action type don't exist, vefiry the folder");
