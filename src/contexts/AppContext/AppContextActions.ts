d6412553112f4b2c2008f4c51538b0703f555fb6
andallves <andrealves10a@gmail.com>
2023-06-13 13:45:02 -0300

    create a enum with all the actions

--- src/contexts/AppContext/AppContextActions.ts
+++ src/contexts/AppContext/AppContextActions.ts
@@ -1,5 +1,11 @@
 export enum AppContextActions {
   NextStep = 'NEXT_STEP',
   PrevStep = 'PREV_STEP',
-  UpdateData = 'UPDATE_DATA',
+  UpdateName = 'UPDATE_NAME',
+  UpdateTel = 'UPDATE_TEL',
+  UpdateEmail = 'UPDATE_EMAIL',
+  UpdateNameBusiness = 'UPDATE_NAME_BUSINESS',
+  UpdateQtdFuncionarios = 'UPDATE_QTD_FUNCIONARIOS',
+  UpdateAboutBusiness = 'UPDATE_ABOUT_BUSINESS',
+  UpdateAboutProject = 'UPDATE_ABOUT_PROJECT',
 }
